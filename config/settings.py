33d1d46b79e63738b6f8d37eb7b74167028a69dc
Андрей Галкин <justscoundrel@yandex.ru>
2026-04-08 18:09:29 +0300

    Первый коммит

--- config/settings.py
+++ config/settings.py
@@ -159,6 +159,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = Path(os.getenv("STATIC_ROOT", BASE_DIR / "staticfiles"))
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
